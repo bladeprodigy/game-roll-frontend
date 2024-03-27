3946e0d6a57cca08489d087f44e2766ee2ab2356
Polmys <127145695+Polmys@users.noreply.github.com>
2024-03-27 08:06:00 +0100

    Pierwsza strona? (#1)
    * Pierwsza strona?
    
    * Piersza strona cala
    
    * Piersza strona cala

---
+++ src/Components/AvatarComponent.tsx
@@ -0,0 +1,10 @@
+import Avatar from '@mui/material/Avatar';
+import CasinoIcon from '@mui/icons-material/Casino';
+
+const AvatarComponent = () => (
+  <Avatar sx={{ m: 10, bgcolor: 'secondary.main', width: 75, height: 75 }}>
+    <CasinoIcon fontSize="large" />
+  </Avatar>
+);
+
+export default AvatarComponent;
